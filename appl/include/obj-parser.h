a95caed254f4aeeb93630e8d948bf62997f3980d
M4RYIT <isabellathemoon@gmail.com>
2022-02-22 17:13:38 +0100

    Vnormals parsing added and code refactored

--- appl/include/obj-parser.h
+++ appl/include/obj-parser.h
@@ -11,22 +11,29 @@ typedef struct obj_mesh
     float *vtexture;
     int vtexture_size;
     int vtexture_count;
-}obj_mesh_t;
 
-static obj_mesh_t* obj_parser_parse(char* obj_path)
+    float *vnormals;
+    int vnormals_size;
+    int vnormals_count;
+} obj_mesh_t;
+
+static obj_mesh_t *obj_parser_parse(char *obj_path)
 {
-    FILE* file = fopen(obj_path, "r");
-    if(file == NULL)
+    FILE *file = NULL;
+    if (fopen_s(&file, obj_path, "r") != 0)
         return NULL;
-    obj_mesh_t* mesh = malloc(sizeof(obj_mesh_t));
 
-    char *buffer[1024];
+    obj_mesh_t *mesh = malloc(sizeof(obj_mesh_t));
+
+    char buffer[1024];
     mesh->vertices_count = 0;
     mesh->vtexture_count = 0;
+    mesh->vnormals_count = 0;
 
-    while (fgets(buffer,1024,file))
+    while (fgets(buffer, 1024, file))
     {
-        if(!strncmp(buffer, "v ", 2)){
+        if (!strncmp(buffer, "v ", 2))
+        {
             ++mesh->vertices_count;
         }
 
@@ -34,42 +41,64 @@ static obj_mesh_t* obj_parser_parse(char* obj_path)
         {
             ++mesh->vtexture_count;
         }
+
+        if (!strncmp(buffer, "vn ", 3))
+        {
+            ++mesh->vnormals_count;
+        }
     }
 
-    mesh->vertices_size = mesh->vertices_count*3;
-    mesh->vtexture_size = mesh->vtexture_count*2;
+    mesh->vertices_size = mesh->vertices_count * 3;
+    mesh->vtexture_size = mesh->vtexture_count * 2;
+    mesh->vnormals_size = mesh->vnormals_count * 3;
 
-    mesh->vertices = malloc(mesh->vertices_size*sizeof(float));
-    mesh->vtexture = malloc(mesh->vtexture_size*sizeof(float));
+    mesh->vertices = malloc(mesh->vertices_size * sizeof(float));
+    mesh->vtexture = malloc(mesh->vtexture_size * sizeof(float));
+    mesh->vnormals = malloc(mesh->vnormals_size * sizeof(float));
 
     rewind(file);
     int v_index = 0;
     int vt_index = 0;
+    int vn_index = 0;
 
-    while(fgets(buffer,1024,file))
+    while (fgets(buffer, 1024, file))
     {
-        char *token = strtok(buffer, " ");
-        if(!strcmp(token,"v"))
+        char *context;
+        char *token = strtok_s(buffer, " ", &context);
+        if (!strcmp(token, "v"))
         {
-            token = strtok(NULL, " ");
+            token = strtok_s(NULL, " ", &context);
             mesh->vertices[v_index + 0] = atof(token);
 
-            token = strtok(NULL, " ");
+            token = strtok_s(NULL, " ", &context);
             mesh->vertices[v_index + 1] = atof(token);
 
-            token = strtok(NULL, " ");
+            token = strtok_s(NULL, " ", &context);
             mesh->vertices[v_index + 2] = atof(token);
-            v_index +=3;
+            v_index += 3;
         }
 
-        if(!strcmp(token,"vt"))
+        if (!strcmp(token, "vt"))
         {
-            token = strtok(NULL, " ");
+            token = strtok_s(NULL, " ", &context);
             mesh->vtexture[vt_index + 0] = atof(token);
 
-            token = strtok(NULL, " ");
+            token = strtok_s(NULL, " ", &context);
             mesh->vtexture[vt_index + 1] = atof(token);
             vt_index += 2;
+        }
+
+        if (!strcmp(token, "vn"))
+        {
+            token = strtok_s(NULL, " ", &context);
+            mesh->vnormals[vn_index + 0] = atof(token);
+
+            token = strtok_s(NULL, " ", &context);
+            mesh->vnormals[vn_index + 1] = atof(token);
+
+            token = strtok_s(NULL, " ", &context);
+            mesh->vnormals[vn_index + 2] = atof(token);
+            vn_index += 3;
         }
     }
 
@@ -77,7 +106,7 @@ static obj_mesh_t* obj_parser_parse(char* obj_path)
     return mesh;
 }
 
-static void obj_parser_free(obj_mesh_t* mesh_to_free)
+static void obj_parser_free(obj_mesh_t *mesh_to_free)
 {
     free(mesh_to_free);
 }
--- test/src/obj-parser-test.c
+++ test/src/obj-parser-test.c
@@ -2,58 +2,73 @@
 #include "clove.h"
 #include "obj-parser.h"
 
-//CLOVE_EXEC_BASE_PATH path to test folder (parent of test.exe)
-//CLOVE_EXEC_PATH path to .exe
-CLOVE_TEST(ObjParserCreatingMeshFromExistingObjFile) {
-   obj_mesh_t* mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
+// CLOVE_EXEC_BASE_PATH path to test folder (parent of test.exe)
+// CLOVE_EXEC_PATH path to .exe
+CLOVE_TEST(ObjParserCreatingMeshFromExistingObjFile)
+{
+   obj_mesh_t *mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
    CLOVE_NOT_NULL(mesh);
    obj_parser_free(mesh);
 }
 
 CLOVE_TEST(ObjReadAllQuadVertex)
 {
-   obj_mesh_t* mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
+   obj_mesh_t *mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
 
    CLOVE_INT_EQ(mesh->vertices_count, 4);
    CLOVE_INT_EQ(mesh->vertices_size, 12);
 
-   CLOVE_FLOAT_EQ(mesh->vertices[0],-1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[1],1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[2],0.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[0], -1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[1], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[2], 0.f);
 
-   CLOVE_FLOAT_EQ(mesh->vertices[3],1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[4],1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[5],0.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[3], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[4], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[5], 0.f);
 
-   CLOVE_FLOAT_EQ(mesh->vertices[6],-1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[7],-1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[8],0.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[6], -1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[7], -1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[8], 0.f);
 
-   CLOVE_FLOAT_EQ(mesh->vertices[9],1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[10],-1.f);
-   CLOVE_FLOAT_EQ(mesh->vertices[11],0.f); 
+   CLOVE_FLOAT_EQ(mesh->vertices[9], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[10], -1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[11], 0.f);
 
    obj_parser_free(mesh);
 }
 
 CLOVE_TEST(ObjReadAllQuadVTexture)
 {
-   obj_mesh_t* mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
+   obj_mesh_t *mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
 
    CLOVE_INT_EQ(mesh->vtexture_count, 4);
    CLOVE_INT_EQ(mesh->vtexture_size, 8);
 
-   CLOVE_FLOAT_EQ(mesh->vtexture[0],1.f);
-   CLOVE_FLOAT_EQ(mesh->vtexture[1],1.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[0], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[1], 1.f);
 
-   CLOVE_FLOAT_EQ(mesh->vtexture[2],0.f);
-   CLOVE_FLOAT_EQ(mesh->vtexture[3],1.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[2], 0.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[3], 1.f);
 
-   CLOVE_FLOAT_EQ(mesh->vtexture[4],0.f);
-   CLOVE_FLOAT_EQ(mesh->vtexture[5],0.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[4], 0.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[5], 0.f);
 
-   CLOVE_FLOAT_EQ(mesh->vtexture[6],1.f);
-   CLOVE_FLOAT_EQ(mesh->vtexture[7],0.f);
-   
+   CLOVE_FLOAT_EQ(mesh->vtexture[6], 1.f);
+   CLOVE_FLOAT_EQ(mesh->vtexture[7], 0.f);
+
+   obj_parser_free(mesh);
+}
+
+CLOVE_TEST(ObjReadAllQuadNormals)
+{
+   obj_mesh_t *mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
+
+   CLOVE_INT_EQ(mesh->vnormals_count, 1);
+   CLOVE_INT_EQ(mesh->vnormals_size, 3);
+
+   CLOVE_FLOAT_EQ(mesh->vnormals[0], 0.f);
+   CLOVE_FLOAT_EQ(mesh->vnormals[1], 0.f);
+   CLOVE_FLOAT_EQ(mesh->vnormals[2], 1.f);
+
    obj_parser_free(mesh);
 }
