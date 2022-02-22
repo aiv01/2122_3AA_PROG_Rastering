35f824d025e530d527616de740d7c882f6e2ace2
AssoAndrea <andrea1944@live.it>
2022-02-22 15:59:34 +0100

    added vertex parsing

--- appl/include/obj-parser.h
+++ appl/include/obj-parser.h
@@ -1,9 +1,12 @@
 #include "vector.h"
 #include <stdio.h>
+#include <string.h>
 
 typedef struct obj_mesh
 {
-    int a;
+    float *vertices;
+    int vertices_size;
+    int vertices_count;
 }obj_mesh_t;
 
 static obj_mesh_t* obj_parser_parse(char* obj_path)
@@ -12,6 +15,44 @@ static obj_mesh_t* obj_parser_parse(char* obj_path)
     if(file == NULL)
         return NULL;
     obj_mesh_t* mesh = malloc(sizeof(obj_mesh_t));
+
+    char *buffer[1024];
+    int vertex_counter = 0;
+
+    while (fgets(buffer,1024,file))
+    {
+        if(!strncmp(buffer, "v ", 2)){
+            vertex_counter++;
+        }
+    }
+
+    mesh->vertices_count = vertex_counter;
+    mesh->vertices_size = vertex_counter*3;
+
+    mesh->vertices = malloc(mesh->vertices_size*sizeof(float));
+    rewind(file);
+    int index = 0;
+
+
+    while(fgets(buffer,1024,file))
+    {
+
+        char *token = strtok(buffer, " ");
+        if(!strcmp(token,"v"))
+        {
+            token = strtok(NULL, " ");
+            mesh->vertices[index + 0] = atof(token);
+
+            token = strtok(NULL, " ");
+            mesh->vertices[index + 1] = atof(token);
+
+            token = strtok(NULL, " ");
+            mesh->vertices[index + 2] = atof(token);
+            index +=3;
+        }
+    }
+
+    fclose(file);
     return mesh;
 }
 
--- test/src/obj-parser-test.c
+++ test/src/obj-parser-test.c
@@ -10,3 +10,28 @@ CLOVE_TEST(ObjParserCreatingMeshFromExistingObjFile) {
    obj_parser_free(mesh);
 }
 
+CLOVE_TEST(ObjReadAllQuadVertex)
+{
+   obj_mesh_t* mesh = obj_parser_parse("bin\\test\\resources\\quad.obj");
+
+   CLOVE_INT_EQ(mesh->vertices_count, 4);
+   CLOVE_INT_EQ(mesh->vertices_size, 12);
+
+   CLOVE_FLOAT_EQ(mesh->vertices[0],-1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[1],1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[2],0.f);
+
+   CLOVE_FLOAT_EQ(mesh->vertices[3],1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[4],1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[5],0.f);
+
+   CLOVE_FLOAT_EQ(mesh->vertices[6],-1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[7],-1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[8],0.f);
+
+   CLOVE_FLOAT_EQ(mesh->vertices[9],1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[10],-1.f);
+   CLOVE_FLOAT_EQ(mesh->vertices[11],0.f); 
+
+   obj_parser_free(mesh);
+}
